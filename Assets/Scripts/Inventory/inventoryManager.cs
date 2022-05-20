48d36e8a632a8303f175c74e6b282bd8b28a1cd0
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-05-20 20:38:39 +0200

    inventory managable

--- Assets/Scripts/Inventory/inventoryManager.cs
+++ Assets/Scripts/Inventory/inventoryManager.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class inventoryManager : MonoBehaviour
 {
     public int food;
     public int wood;
     public int stone;
+
+    public Text tfood, twood, tstone;
+
+    public void InventoryUpdate()
+    {
+        UIManager.ChangeText(tfood, food);
+        UIManager.ChangeText(twood, wood);
+        UIManager.ChangeText(tstone, stone);
+    }
 }
