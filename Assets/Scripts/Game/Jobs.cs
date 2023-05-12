9cf1417987f201380a0a509c91064f709c79c642
psyjr11 <psyjr11@nottingham.ac.uk>
2023-05-12 22:37:17 +0100

    AddedChildFeatureToJobs

--- Assets/Scripts/Game/Jobs.cs
+++ Assets/Scripts/Game/Jobs.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum JOB { Farmer, Engineer, Scientist, Builder  /* To be detemined */ }
-public enum EMPLOYMENT { Unemployed, Employed };
+public enum EMPLOYMENT { Unemployed, Employed, Child }; //Added Child
 
 [System.Serializable]
 public class Jobs
@@ -16,5 +16,8 @@ public class Jobs
             default:
                 break;
         }
+
+        //If age of person is below 18, they are added to the child class, of which is not Employed or Unemployed.
+        if(p.age > 18) { p.Employment = EMPLOYMENT.Child; }
     }
 }
